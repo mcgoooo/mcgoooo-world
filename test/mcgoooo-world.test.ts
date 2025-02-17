80d15091e1bf14ffbd17e418370814591595369a
igberty@gmail.com <ahilton@odeko.com>
2025-02-17 02:15:40 +0000

    change order

---
+++ test/mcgoooo-world.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as McgooooWorld from '../lib/mcgoooo-world-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/mcgoooo-world-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new McgooooWorld.McgooooWorldStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
