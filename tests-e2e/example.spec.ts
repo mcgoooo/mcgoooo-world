98c18f580a18c1d08ac87180456e623a7ae394f9
igberty@gmail.com <ahilton@odeko.com>
2025-07-16 16:51:08 +0100

    Adjust playwright config
    - enable videos on failure
    - enable trace
    - disable retries

--- tests-e2e/example.spec.ts
+++ tests-e2e/example.spec.ts
@@ -1,10 +1,10 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/playwright/);
 });
 
 test('get started link', async ({ page }) => {
