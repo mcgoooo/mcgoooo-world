b0e91057d27e45cd3134722b82b5a246b966346f
igberty@gmail.com <ahilton@odeko.com>
2025-08-05 17:36:12 +0100

    break the e2e, to show process

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -9,7 +9,7 @@ describe("Home", () => {
     render(<Home />);
 
     const heading = screen.getByRole("heading", {
-      name: /ola/i,
+      name: /BREAK/i,
     });
 
     expect(heading).toBeInTheDocument();
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1>OLA</h1>
+      <h1>BREAK TIIII</h1>
       <br/>
-      <h2>BOM DIA TO YOU ALL</h2>
+      <h2>dsijsdkjlkjYOU ALL</h2>
     </div>
   );
 }
