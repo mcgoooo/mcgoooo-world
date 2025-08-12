bb9dcdf56d7d28dd40c5cf070daf1358ad025b7f
igberty@gmail.com <ahilton@odeko.com>
2025-08-12 17:44:20 +0100

    Add domain for spotify env
    THis is so local dev works

--- src/requests/currentlyPlaying.tsx
+++ src/requests/currentlyPlaying.tsx
@@ -7,13 +7,14 @@ export default async function currentlyPlaying() {
       const allCookies = (await cookies()).getAll()
     console.log("All cookies:", allCookies);
   const token = (await cookies()).get("spotify_access_token")?.value;
+  const url =    process.env.MCGOOOO_SPOTIFY_DOMAIN + "/currently-playing"
+  console.log("URL",url);
   if (!token) {
 
     throw new Error("No Spotify access token found in cookies");
   }
-  const currentlyPlaying = await fetch(
-    "https://spotify.mcgoooo.world/currently-playing",
-    {
+  const currentlyPlaying = await fetch(url
+    ,{
       method: "get",
       headers: {
         Cookie: "spotify_access_token=" + token!,
